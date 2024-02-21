333b7f648bd86cb2c0e8696889869d1e7b5f5c4f
vilasboasfabio <vilasboasfabio564@gmail.com>
2024-02-21 09:37:16 -0300

    add: task form input

--- App.jsx
+++ App.jsx
@@ -1,21 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import React from 'react';
 
 export default function App() {
+
+  const [task, setTask] = useState('');
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.form}>
+    <TextInput
+      placeholder="Digite uma nova tarefa"
+      style={styles.inputline}
+      onChangeText={setTask}
+      value={task}
+    />
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.text}>Adicionar</Text>
+    </TouchableOpacity>
+  </View>
+  <StatusBar style="auto" />
+</View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    padding: 12,
+    marginTop: 24,
+  },
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 6,
+  },
+  inputline: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    padding: 2,
+    width: '75%',
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 2,
+    borderRadius: 4,
+  },
+  text: {
+    color: 'white',
   },
 });
